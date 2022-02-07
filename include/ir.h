36fccd6079306b245241425cd866f4baa38580a9
eliphatfs <flandre@scarletx.cn>
2022-02-07 14:35:39 +0800

    More instructions.

--- include/ir.h
+++ include/ir.h
@@ -10,6 +10,14 @@
 
 namespace yapyjit {
 	// Base class for all instructions
+	enum class BinaryOp {
+		Add = 1, Sub = 2, Mult = 3, MatMult = 4, Div = 5, Mod = 6, Pow = 7,
+		LShift = 8, RShift = 9, BitOr = 10, BitXor = 11, BitAnd = 12,
+		FloorDiv = 13
+	};
+	enum class UnaryOp {
+		Invert = 1, Not = 2, UAdd = 3, USub = 4
+	};
 	class Instruction {
 	public:
 		virtual ~Instruction() = default;
@@ -21,16 +29,45 @@ namespace yapyjit {
 
 	class ReturnIns : public Instruction {
 	public:
-		int local_id;
-		ReturnIns(int _local_id) : local_id(_local_id) {}
+		int src;
+		ReturnIns(int local_id) : src(local_id) {}
 	};
 
 	class MoveIns : public Instruction {
 	public:
-		int dst_local_id;
-		int src_local_id;
-		MoveIns(int _dst_local_id, int _src_local_id)
-			: dst_local_id(_dst_local_id), src_local_id(_src_local_id) {}
+		int dst;
+		int src;
+		MoveIns(int dst_local_id, int src_local_id)
+			: dst(dst_local_id), src(src_local_id) {}
+	};
+
+	class BinOpIns : public Instruction {
+	public:
+		int dst, left, right;
+		BinaryOp op;
+		BinOpIns(int dst_local_id, BinaryOp op_, int left_local_id, int right_local_id)
+			: dst(dst_local_id), left(left_local_id), right(right_local_id), op(op_) {}
+	};
+
+	class UnaryOpIns : public Instruction {
+	public:
+		int dst, operand;
+		UnaryOp op;
+		UnaryOpIns(int dst_local_id, UnaryOp op_, int operand_local_id)
+			: dst(dst_local_id), operand(operand_local_id), op(op_) {}
+	};
+
+	class JumpIns : public Instruction {
+	public:
+		LabelIns * target;
+		JumpIns(LabelIns * target_): target(target_) {}
+	};
+
+	class JumpTruthyIns : public JumpIns {
+	public:
+		int cond;
+		JumpTruthyIns(LabelIns* target_, int cond_local_id)
+			: JumpIns(target_), cond(cond_local_id) {}
 	};
 
 	class Function {
