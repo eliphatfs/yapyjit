24e784a94ceedfe0f4ffdc6aa706da7e952d9d19
eliphatfs <flandre@scarletx.cn>
2022-02-23 22:21:38 +0800

    Better managedment of compiler flags.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 import setuptools
 import glob
+import platform
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+cc_args = []
+cl_args = []
+if platform.system() == 'Windows':
+    cc_args += ['/DEBUG', '/Z7']
+    cl_args += ['/DEBUG']
+else:
+    cc_args += ['-g']
 
 
 setuptools.setup(
@@ -21,8 +31,9 @@ setuptools.setup(
         glob.glob("src/**/*.c", recursive=True)
         + glob.glob("src/**/*.cpp", recursive=True)
         + ["mir/mir.c", "mir/mir-gen.c"],
-        extra_compile_args=['/DEBUG', '/Z7', '/DMIR_INTERP_TRACE=1', '/DNO_COMBINE=1'],
-        extra_link_args=['/DEBUG']
+        define_macros=[("NO_COMBINE", "1"), ("MIR_INTERP_TRACE", "1")],
+        extra_compile_args=cc_args,
+        extra_link_args=cl_args
     )],
     include_dirs=["include", "mir"],
     classifiers=[
