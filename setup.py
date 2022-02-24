0e1b7b500829dea4018541e61874fa04bd9b3be8
eliphatfs <flandre@scarletx.cn>
2022-02-24 11:42:25 +0800

    Drop support for python 3.6.

--- setup.py
+++ setup.py
@@ -41,5 +41,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: Apache Software License"
     ],
-    python_requires='~=3.6'
+    python_requires='~=3.7'
 )
