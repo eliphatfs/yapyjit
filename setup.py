6035818fbdf17338b89024d5d44a35207c86720c
eliphatfs <flandre@scarletx.cn>
2022-02-24 18:21:50 +0800

    Filter cpp standard option for gcc.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from setuptools.command.build_ext import build_ext
 import glob
 import platform
 
@@ -17,6 +18,20 @@ else:
     # TODO: change all occurrences
 
 
+class build_ext_subclass(build_ext):
+    def build_extensions(self):
+        original_compile = self.compiler._compile
+        def new_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+            if src.endswith('.c'):
+                extra_postargs = [s for s in extra_postargs if "c++17" not in s]
+            return original_compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+        self.compiler._compile = new_compile
+        try:
+            build_ext.build_extensions(self)
+        finally:
+            del self.compiler._compile
+
+
 setuptools.setup(
     name="yapyjit",
     version="0.0.1a1",
@@ -27,6 +42,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com",
     include_package_data=True,
+    cmdclass={"build_ext": build_ext_subclass},
     ext_modules=[setuptools.Extension(
         "yapyjit",
         glob.glob("src/**/*.c", recursive=True)
