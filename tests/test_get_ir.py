3f95a16f2c6dcf09a599d89578fde24be77c6fe9
eliphatfs <flandre@scarletx.cn>
2022-10-02 16:55:39 +0800

    Test for new IR.

---
+++ tests/test_get_ir.py
@@ -0,0 +1,27 @@
+import unittest
+import yapyjit
+
+
+print_ir = False
+
+
+def zero():
+    return 0
+
+
+def testfun():
+    return 1 < 2 < 3 + zero()
+
+
+class TestGetIR(unittest.TestCase):
+
+    def test_basic(self):
+        ir = yapyjit.get_ir(testfun)
+        self.assertTrue(b'zero' in ir)
+        if print_ir:
+            print(ir)
+
+
+if __name__ == '__main__':
+    print_ir = True
+    unittest.main()
