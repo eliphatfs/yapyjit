e6e5477f14e24096fe96a23f2a5d99afb4d1dc0b
eliphatfs <flandre@scarletx.cn>
2022-10-07 09:59:52 +0800

    Test CI.

--- tests/test_get_ir.py
+++ tests/test_get_ir.py
@@ -9,17 +9,26 @@ def zero():
     return 0
 
 
-def testfun():
+def test_1():
     return 1 < 2 < 3 + zero()
+
+
+def test_2(n):
+    s = 0
+    for i in range(n + 5):
+        s += i
+    return s
 
 
 class TestGetIR(unittest.TestCase):
 
     def test_basic(self):
-        ir = yapyjit.get_ir(testfun)
+        ir = yapyjit.get_ir(test_1)
+        ir2 = yapyjit.get_ir(test_2)
         self.assertTrue(b'zero' in ir)
         if print_ir:
             print(yapyjit.pprint_ir(ir))
+            print(yapyjit.pprint_ir(ir2))
 
 
 if __name__ == '__main__':
